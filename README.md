d2c5911c4950fcc7fb5a7752285ced314ca03126
Carlos <carlos@MacBook-Pro-de-Carlos.local>
2020-05-15 01:52:58 -0300

    crud

--- README.md
+++ README.md
@@ -21,5 +21,44 @@ Installing symfony/skeleton (v5.0.99)
   pacotes:
 
   docker exec -it symfony_php composer require annotation
-  docker exec -it symfony_php composer require orm-pack
+  docker exec -it symfony_php composer require symfony/orm-pack
   docker exec -it symfony_php composer require maker
+  docker exec -it symfony_php composer require serializer
+
+  docker exec -it symfony_php composer require security
+  docker exec -it symfony_php composer require firebase/php-jwt
+
+
+  1 - Criar uma entidade (entity) - A estrutura da tabela
+  docker exec -it symfony_php bin/console make:entity
+
+  2 - * antes é importante configurar o .env com os dados da conexao, o nome do banco informado nao precisa está criado, pode rodar docker exec -it bin/console doctrine:database:create
+  DATABASE_URL=mysql://root:senha123@127.0.0.1:3306/baselab?serverVersion=5.7
+
+  3 - Criar a migrate da entity para criar a tabela no banco
+  docker exec -it symfony_php bin/console make:migration
+
+  4 - depois criar a estrutura acima no banco (criar a tabela)
+  docker exec -it symfony_php bin/console doctrine:migrations:migrate
+  
+  resultado com sucesso: "CREATE TABLE usuario (id INT AUTO_INCREMENT NOT NULL, nome VARCHAR(150) NOT NULL, login VARCHAR(150) NOT NULL, senha VARCHAR(150) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, hash VARCHAR(150) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB"
+
+
+## criar controller para acessar a estrutura acima
+
+1 - criar um controller 
+docker exec -it symfony_php bin/console make:controller
+- Informa o nome ex Usuario
+
+2 - Depois de definir as routas pode usar o comando para listar todas criadas
+docker exec -it symfony_php bin/console debug:router
+
+saida 
+
+  _preview_error   ANY         ANY      ANY    /_error/{code}.{_format}  
+  usuario_index    GET         ANY      ANY    /usuario/                 
+  usuario_view     GET         ANY      ANY    /usuario/{id}             
+  usuario_create   POST        ANY      ANY    /usuario/{id}             
+  usuario_update   PUT|PATCH   ANY      ANY    /usuario/{id}             
+  usuario_delete   DELETE      ANY      ANY    /usuario/{id} 
+
---
+++ application/src/Entity/Usuario.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UsuarioRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UsuarioRepository::class)
+ */
+class Usuario
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    private $nome;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    private $login;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    private $senha;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    private $hash;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getLogin(): ?string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): self
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    public function getSenha(): ?string
+    {
+        return $this->senha;
+    }
+
+    public function setSenha(string $senha): self
+    {
+        $this->senha = $senha;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getHash(): ?string
+    {
+        return $this->hash;
+    }
+
+    public function setHash(string $hash): self
+    {
+        $this->hash = $hash;
+
+        return $this;
+    }
+}
